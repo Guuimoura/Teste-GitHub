ecd453a1a7036ca6fbee5c6119196bdb4e75f6c5
Guuimoura <guicoelho.moura@gmail.com>
2024-03-25 23:16:02 -0300

    Meu primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Gui from "./img/guiguiba.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>O GUIGUIBA É SHOW</h1>
+      <img src={Gui} alt="Guiguiba" title="Amigão" />
+      <h2>MEU AMIGÃO </h2>
+    </div>
+  );
+}
+
+export default App;
+
